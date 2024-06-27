cfbcfcca8f75d63cf0b3310d6a7fb645d72b7e16
Hall Development <hallldevelopment@gmail.com>
2024-06-27 19:24:55 +0400

    Update config.lua

--- hd-pedspawner/config.lua
+++ hd-pedspawner/config.lua
@@ -10,4 +10,14 @@ Config.Distance = 30.0 -- المسافة الي البوت يرسبن منها
 Config.MinusOne = true -- الأفضل عدم لمسه
 
 Config.PedList = {
+	{
+	model = "g_m_m_armboss_01", -- كود البوت
+	coords = vector3(1845.93, 2587.71, 45.67), -- الأحداثيات
+	heading = 275.44,  -- vector 4 الرقم الاخير لو كنت تستعمل الـ
+	gender = "male", -- جنس البوت
+	animDict = "missbigscore2aig_3", -- الأسم الكبير في موقع الأنيميشينات
+	animName = "wait_for_van_c", -- الأسم الي تحت الأسم الكبير الي في موقع الأنيميشينات
+	isRendered = false, -- لا تلمسه ابدًا
+	ped = nil, -- لا تلمسه ابدًا
+},
 }
